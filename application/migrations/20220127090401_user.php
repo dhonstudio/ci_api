063fe2bbdcd165d83ed181231205bb25fb245d97
dhonstudio <doon13@gmail.com>
2022-02-28 08:57:29 +0700

    Adding user device (log)

--- application/migrations/20220127090401_user.php
+++ application/migrations/20220127090401_user.php
@@ -12,19 +12,43 @@ class Migration_User {
     
     public function up()
     {
-        $this->migration->dhondb->table = 'user_ci';
-        $this->migration->dhondb->ai()->field('id', 'INT');
-        $this->migration->dhondb->constraint('255')->unique()->field('email', 'VARCHAR');
-        $this->migration->dhondb->constraint('200')->field('fullName', 'VARCHAR');
-        $this->migration->dhondb->constraint('32')->field('auth_key', 'VARCHAR');
-        $this->migration->dhondb->constraint('255')->field('password_hash', 'VARCHAR');
-        $this->migration->dhondb->constraint('255')->default(null)->field('password_reset_token', 'VARCHAR', 'nullable');
-        $this->migration->dhondb->constraint('6')->default('10')->field('status', 'smallint');
-        $this->migration->dhondb->field('created_at', 'INT');
-        $this->migration->dhondb->field('updated_at', 'INT');
-        $this->migration->dhondb->constraint('255')->default(null)->field('verification_token', 'VARCHAR', 'nullable');
-        $this->migration->dhondb->add_key('id');
-        $this->migration->dhondb->create_table();
+        // $this->migration->dhonmigrate->table = 'user_ci';
+        // $this->migration->dhonmigrate->ai()->field('id', 'INT');
+        // $this->migration->dhonmigrate->constraint('255')->unique()->field('email', 'VARCHAR');
+        // $this->migration->dhonmigrate->constraint('200')->field('fullName', 'VARCHAR');
+        // $this->migration->dhonmigrate->constraint('32')->field('auth_key', 'VARCHAR');
+        // $this->migration->dhonmigrate->constraint('255')->field('password_hash', 'VARCHAR');
+        // $this->migration->dhonmigrate->constraint('255')->default(null)->field('password_reset_token', 'VARCHAR', 'nullable');
+        // $this->migration->dhonmigrate->constraint('6')->default('10')->field('status', 'smallint');
+        // $this->migration->dhonmigrate->field('created_at', 'INT');
+        // $this->migration->dhonmigrate->field('updated_at', 'INT');
+        // $this->migration->dhonmigrate->constraint('255')->default(null)->field('verification_token', 'VARCHAR', 'nullable');
+        // $this->migration->dhonmigrate->add_key('id');
+        // $this->migration->dhonmigrate->create_table();
+
+        $this->migration->dhonmigrate->table = 'user_device';
+        $this->migration->dhonmigrate->ai()->field('id', 'INT');
+        $this->migration->dhonmigrate->default('0')->field('id_user', 'INT');
+        $this->migration->dhonmigrate->default('0')->field('id_device', 'INT');
+        $this->migration->dhonmigrate->default('0')->field('id_address', 'INT');
+        $this->migration->dhonmigrate->field('stamp', 'INT');
+        $this->migration->dhonmigrate->field('last_login', 'INT');
+        $this->migration->dhonmigrate->add_key('id');
+        $this->migration->dhonmigrate->create_table();
+
+        $this->migration->dhonmigrate->table = 'devices';
+        $this->migration->dhonmigrate->ai()->field('id_device', 'INT');
+        $this->migration->dhonmigrate->constraint('1000')->unique()->field('htmlentities', 'VARCHAR', 'nullable');
+        $this->migration->dhonmigrate->constraint('100')->field('device_name', 'VARCHAR', 'nullable');
+        $this->migration->dhonmigrate->add_key('id_device');
+        $this->migration->dhonmigrate->create_table();
+
+        $this->migration->dhonmigrate->table = 'addresses';
+        $this->migration->dhonmigrate->ai()->field('id_address', 'INT');
+        $this->migration->dhonmigrate->constraint('50')->unique()->field('ip_address', 'VARCHAR', 'nullable');
+        $this->migration->dhonmigrate->constraint('1500')->field('ip_info', 'VARCHAR', 'nullable');
+        $this->migration->dhonmigrate->add_key('id_address');
+        $this->migration->dhonmigrate->create_table();
     }
 
     public function change()
