0133dfeda60314e6be08034f542067b2cb1931ad
dhonstudio <doon13@gmail.com>
2022-03-11 15:05:25 +0700

    Renew method to migrate

--- application/controllers/Migrate.php
+++ application/controllers/Migrate.php
@@ -6,21 +6,30 @@ class Migrate extends CI_Controller
 	{
 		parent::__construct();
 
+        /*
+        | -------------------------------------------------------------------
+        |  Set up this API User Database, Migration Database, and Migration Class
+        | -------------------------------------------------------------------
+        */
+        $api_db             = 'project';
+        $this->database     = 'project';
+        $this->migration    = 'api';
+
         require_once __DIR__ . '/../../assets/ci_libraries/DhonAuth.php';
         require_once __DIR__ . '/../../assets/ci_libraries/DhonJSON.php';
+        require_once __DIR__ . '/../../assets/ci_libraries/DhonMigrate.php';
 		$this->dhonauth     = new DhonAuth;
 		$this->dhonjson     = new DhonJSON;
 
-        $this->dhonauth->auth('project');
+        $this->dhonauth->auth($api_db);
     }
 
     public function index()
     {
-        require_once __DIR__ . '/../../assets/ci_libraries/DhonMigrate.php';
-        $this->dhonmigrate = new DhonMigrate('project');
-
+        $this->dhonmigrate = new DhonMigrate($this->database);
         $this->dhonmigrate->version = 20220127090401;
-        $this->dhonmigrate->migrate('user');
+        $this->dhonmigrate->migrate($this->migration);
+
         $response   = 'Migration success';
         $status     = '200';
         $this->dhonjson->send($response, $status);
--- application/migrations/20220127090401_api.php
+++ application/migrations/20220127090401_api.php
@@ -6,8 +6,10 @@ class Migration_Api {
 	{
         $this->migration =& get_instance();
         
+        $this->database = $database;
+        $this->dev      = false;
         require_once __DIR__ . '/../../assets/ci_libraries/DhonMigrate.php';
-		$this->migration->dhonmigrate = new DhonMigrate($database);
+		$this->migration->dhonmigrate = new DhonMigrate($this->database);
     }
     
     public function up()
@@ -21,21 +23,22 @@ class Migration_Api {
         $this->migration->dhonmigrate->create_table();
 
         $this->migration->dhonmigrate->insert(['username' => 'admin', 'password' => password_hash('admin', PASSWORD_DEFAULT)]);
+        
+        if ($this->dev == false) $this->_dev();
+    }
+
+    private function _dev()
+    {
+        $this->migration->dhonmigrate = new DhonMigrate($this->database.'_dev');
+        $this->dev = true;
+        $this->up();
     }
 
     public function change()
     {
-        $this->migration->dhonmigrate->table = 'api_users';
-        $this->migration->dhonmigrate->constraint('10')->field(['usernamed', 'usernamp'], 'VARCHAR');
-        // $this->migration->dhonmigrate->constraint('100')->field('usernames', 'VARCHAR');
-        // $this->migration->dhonmigrate->add_field();
-        $this->migration->dhonmigrate->change_field();
     }
 
     public function drop()
     {
-        $this->migration->dhonmigrate->table = 'api_users';
-        $this->migration->dhonmigrate->drop_field('usernamed');
-        $this->migration->dhonmigrate->drop_field('usernames');
     }
 }
