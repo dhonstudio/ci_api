e2aa3457edcddb8d6a5c7526273c06aff80bf309
dhonstudio <doon13@gmail.com>
2022-03-23 08:46:32 +0700

    Recap migration file; add user_yii

--- application/controllers/Migrate.php
+++ application/controllers/Migrate.php
@@ -13,7 +13,7 @@ class Migrate extends CI_Controller
         */
         $api_db             = 'project';
         $this->database     = 'project';
-        $this->migration    = 'api';
+        $this->migration    = 'project';
 
         require_once __DIR__ . '/../../assets/ci_libraries/DhonAuth.php';
         require_once __DIR__ . '/../../assets/ci_libraries/DhonJSON.php';
