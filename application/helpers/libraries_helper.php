6593588ee630dc69c1ed74562a24df81a05a0b6f
dhonstudio <doon13@gmail.com>
2022-04-07 07:43:48 +0700

    Fixed some helper

--- application/helpers/libraries_helper.php
+++ application/helpers/libraries_helper.php
@@ -4,5 +4,6 @@ $ci = get_instance();
 
 require_once __DIR__ . '/../../assets/ci_libraries/DhonAuth.php';
 require_once __DIR__ . '/../../assets/ci_libraries/DhonJSON.php';
+require_once __DIR__ . '/../../assets/ci_libraries/DhonMigrate.php';
 $ci->dhonauth = new DhonAuth;
 $ci->dhonjson = new DhonJSON;
