5beb3d2fa07fe6e37ff3b5c4fac212afa6b7cd31
dhonstudio <doon13@gmail.com>
2022-04-07 15:23:12 +0700

    Manage development database migration

--- application/migrations/20220127090401_project.php
+++ application/migrations/20220127090401_project.php
@@ -160,7 +160,7 @@ class Migration_Project {
         // $this->migration->dhonmigrate->add_key('id');
         // $this->migration->dhonmigrate->create_table();
 
-        if ($this->dev == false) $this->_dev();
+        if (ENVIRONMENT != 'development') if ($this->dev == false) $this->_dev();
     }
 
     private function _dev()
